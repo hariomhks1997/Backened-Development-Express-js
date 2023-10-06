56f28ffb6272ec404359e9cf4070cd00eaca6065
Hariom Singh <hariomhks1997@gmail.com>
2023-10-06 15:05:52 +0530

    8.5 Writing and reading from a file

--- app.js
+++ app.js
@@ -26,11 +26,14 @@ const server=http.createServer((req,res)=>{
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
+      
       fs.writeFileSync('message.txt', message);
+      res.statusCode = 302;
+    
+      res.setHeader('Location', '/');
+      return res.end();
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+   
   }
 //console.log(res.url,req.method,req.headers);
   // console.log(req);
