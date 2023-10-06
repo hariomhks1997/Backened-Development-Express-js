4a2772b32cc18a04b4bb7b84091bc2ef70c96477
Hariom Singh <hariomhks1997@gmail.com>
2023-10-06 14:34:53 +0530

    8.3 Writing and reading from a file

--- app.js
+++ app.js
@@ -18,7 +18,17 @@ const server=http.createServer((req,res)=>{
       return res.end();
     }
     if (url==='/message'&& method==="POST") {
-      fs.writeFileSync('message.txt', 'DUMMY');
+      const body = [];
+      req.on('data', (chunk) => {
+         console.log(chunk);
+         body.push(chunk);
+       });
+       req.on('end', () => {
+         const parsedBody = Buffer.concat(body).toString();
+         console.log(parsedBody)
+         
+       });
+       fs.writeFileSync('message.txt',"DUMMY");
       res.statusCode =302;
       res.setHeader('Location', '/');
       return res.end();
