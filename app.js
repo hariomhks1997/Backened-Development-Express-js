65ec1c6fbf7462b2ca57cde481cf17567d0ba2e2
Hariom Singh <hariomhks1997@gmail.com>
2023-10-06 13:18:38 +0530

    9.1 Writing and reading from a file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http=require("http/about");
+const http=require("http");
 // function rqListener(req,res){
 
 // }
@@ -7,8 +7,16 @@ const http=require("http/about");
 
 // });
 const server=http.createServer((req,res)=>{
-    //console.log(res.url,req.method,req.headers);
-   //console.log(req)
+   const url=req.url;
+   if (url === '/') {
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title><head>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+    }
+//console.log(res.url,req.method,req.headers);
+   //console.log(req);
    //process.exit();
    res.setHeader('Content-Type',"text/html")
    res.write('<html>');
@@ -18,4 +26,4 @@ const server=http.createServer((req,res)=>{
    res.end();
    
 });
-server.listen(4000);
+server.listen(3000);
