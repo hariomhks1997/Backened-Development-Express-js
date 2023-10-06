2fadf0e28314ec83a7245172686b562e650bae96
Hariom Singh <hariomhks1997@gmail.com>
2023-10-06 12:25:06 +0530

    2.1 Sending response

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const http=require("http");
 
 // });
 const server=http.createServer((req,res)=>{
-    console.log("res")
+    //console.log(res.url,req.method,req.headers);
+   //console.log(req)
+   //process.exit();
+   res.setHeader('Content-Type',"text/html")
+   res.write('<html>');
+   res.write('<head><title>My First Page</title></head>')
+   res.write("<body><h1>Hello From My Node.js</h1></body>")
+   res.write('</html>');
+   res.end();
+   
 });
 server.listen(3000);
