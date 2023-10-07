93697cd60a68503e5bce26e887192d8a7d41bfbf
Hariom Singh <hariomhks1997@gmail.com>
2023-10-07 21:01:39 +0530

    2.1 Intro to Middlewares

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-
-
 const express=require('express');
 const app=express();
-app.use((req, res, next)=>{
-    console.log("in the middleware"); 
-    next();
+app.use("/",(req, res, next)=>{
+    console.log("this always runs"); 
+   next()
+    
 })
-app.use((req, res, next)=>{
+app.use("/add-product",(req, res, next)=>{
     console.log("in another middleware"); 
-    res.send("hello from express")
+    res.send("<h1>Add product</h1>")
+    
+})
+app.use("/",(req, res, next)=>{
+    console.log("in another middleware"); 
+    res.send("<h1>hello from express</h1>")
     
 })
 
