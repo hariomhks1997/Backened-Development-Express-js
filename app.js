a9ae0607dff6f2ba4236e8bee562e2893f3033e1
Hariom Singh <hariomhks1997@gmail.com>
2023-10-07 18:23:37 +0530

    1.3 Lets get started with Express

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use((req, res, next)=>{
 })
 app.use((req, res, next)=>{
     console.log("in another middleware"); 
+    res.send("hello from express")
 })
 const server=http.createServer(app); 
 
